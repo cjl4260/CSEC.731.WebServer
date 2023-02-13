5394dbf66846c98671375af1252cdb3b48c31429
cjl4260 <60491779+cjl4260@users.noreply.github.com>
2023-02-12 20:56:10 -0500

    Add files via upload

--- Parse_HTTP.py
+++ Parse_HTTP.py
@@ -6,12 +6,8 @@ Description: Parses an HTTP request to check its validity
 '''
 
 
-'''
-Imports:
-Will need to import sys to get file argument
-Will also need to import file for Responses
-
-'''
+import sys
+import Responses
 
 '''
 Function #1
@@ -21,6 +17,12 @@ Get input from user
     Using sys get arg from the command line
     Do a check to make sure the arg exists and print the correct usage if it is not
 '''
+def get_input():
+    if len(sys.argv) != 2:
+         print("HTTP file not specified\n Usage: Parse_HTTP.py <path to file>")
+         exit(1)
+    else:
+        return open(sys.argv[1], 'rb').readlines()
 
 '''
 Function #2
@@ -31,6 +33,20 @@ Verify Method
     If not return a 400 error and exit
 '''
 
+def validate_method(method):
+    valid_methods = [
+        "GET",
+        "POST",
+        "PUT",
+        "DELETE",
+        "CONNECT",
+        "HEAD"
+    ]
+
+    if method.upper() not in valid_methods:
+        Responses.Response_Code_400()
+
+
 '''
 Function #3
 
@@ -39,6 +55,10 @@ Verify version
     Verify that the http version is 1.1
     If not return 400 error and exit 
 '''
+
+def validate_version(version):
+    if http_version != "HTTP/1.1":
+        Responses.Response_Code_400()
 
 '''
 Function #4
@@ -50,6 +70,17 @@ Verify request line syntax
     If not return 400 error and exit 
 '''
 
+def validate_request_line(request_line):
+    if request_line.count(" ") != 2:
+        Responses.Response_Code_400()
+    
+    split_request_line = request_line.split(" ")
+    method = split_request_line[0]
+    validate_method(method)
+
+    version = split_request_line[2]
+    validate_version(version)
+
 '''
 Function #5
 
@@ -58,6 +89,13 @@ Verify a single header
     Verify that this header is valid
     If not return 400 error and exit
 '''
+
+def validate_header(header):
+
+    value = header.replace(" ", "").split(":")[0]
+    if value.upper() == "HOST":
+        return 0
+    return 1
 
 '''
 Function #6
@@ -68,6 +106,12 @@ Verify all headers
     If not return 400 error and exit
     '''
 
+def validate_headers(headers):
+    valid = 0
+    for header in headers:
+        valid += validate_header(header)
+    if valid != 1:
+        Responses.Response_Code_400()
 '''
 Function #7
 Parse out fields
@@ -75,3 +119,27 @@ Parse out fields
     Parse out headers and request line
     Send them to the other functions for validation
     '''
+def parse_request_fields(request):
+    parsed = ""
+    for line in request:
+        parsed += str(line)[2:-1]
+
+    if parsed.count("\\r\\n\\r\\n") != 1:
+        Responses.Response_Code_400()
+    
+    request_line = parsed.split("\r\n\r\n")[0].split("\r\n")[0]
+    validate_request_line(request_line)
+
+    headers = request_str.split("\r\n\r\n")[0].split("\r\n")[1:]
+    validate_headers(headers)
+
+    Responses.Response_Code_200()
+
+def main():
+    try:
+        file = get_input()
+        parse_request_fields(file)
+    except Exception:
+        Responses.Response_Code_500()
+
+main()
